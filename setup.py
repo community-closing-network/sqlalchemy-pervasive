0f7e24d43f4fa70379baa31ff9854a01ae1e1229
Charles Hamilton <chamilton@dyercpa.com>
2019-09-10 08:43:08 -0400

    Refactored / 2to3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-execfile(os.path.join(here, 'sqlalchemy_pervasive', '_version.py'))
+exec(compile(open(os.path.join(here, 'sqlalchemy_pervasive', '_version.py')).read(), os.path.join(here, 'sqlalchemy_pervasive', '_version.py'), 'exec'))
 
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
